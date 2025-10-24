f639798a0b485a1cc1024f332264355b577ea1b6
akash <kushi.akee@gmail.com>
2025-10-24 15:49:23 +0530

    newly modified

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ def add(a, b):
     return a + b
 
 if __name__ == "__main__":
-    print("Hello from Jenkins + Python!")
+    print("Hello from Jenkins & Python!")
     print("2 + 3 =", add(2, 3))
 
 def sub(a,b):
